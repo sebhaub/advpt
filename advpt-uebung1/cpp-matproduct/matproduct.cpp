015855065dac148b7f5d6bd2506cbea84f362878
Sebastian Haubner <sebastian.haubner@fau.de>
2014-11-11 15:15:04 +0100

    - Fixed input method - added array operator - added equals operator - added * operator

--- advpt-uebung1/cpp-matproduct/matproduct.cpp
+++ advpt-uebung1/cpp-matproduct/matproduct.cpp
@@ -4,7 +4,11 @@
 #include <string>
 #include <sstream>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::streamsize;
+using std::numeric_limits;
+using std::endl;
 
 class Matrix{
 public:
@@ -12,9 +16,9 @@ public:
 		assert(rows > 0 && cols > 0);
 		this->rows = rows;
 		this->columns = cols;
-		this->cells = new int[rows*cols];
+		this->cells = new long[rows*cols];
 		for (int i = 0; i < rows*cols; i++){
-			cells[i] = 2;
+			cells[i] = 0;
 		}
 	}
 
@@ -22,45 +26,114 @@ public:
 		delete[] cells;
 	}
 
-	friend std::ostream& operator<<(std::ostream& os, const Matrix& obj)
-	{
-		for (int i = 0; i < obj.rows; i++){
-			for (int j = 0; j < obj.columns; j++){
-				os << obj.cells[i * obj.columns + j] + " a ";
+
+	Matrix Matrix::operator*(const int &other) {
+		Matrix result(this->rows, this->columns);
+		for (unsigned int i = 0; i < this->rows; i++){
+			for (unsigned int j = 0; j < this->columns; j++){
+				this->cells[i*this->columns + j] *= other;
 			}
-			os << "\n";
+
 		}
-		return os;
+		return result;
 	}
+
+	Matrix Matrix::operator*(const Matrix &other) {
+		assert(this->columns == other.rows);
+
+		Matrix neu(this->rows, other.columns);
+
+		for (unsigned int i = 0; i < this->rows; i++){
+			for (unsigned int j = 0; j < other.columns; j++){
+				long sum = this->cells[i*this->columns] * other.cells[j];
+				for (unsigned int k = 1; k < this->columns; k++){
+					sum += this->cells[i*this->rows + k] * other.cells[k*this->rows + j];
+				}
+				neu.cells[i*this->rows + j] = sum;
+			}
+		}
+		return neu;
+	}
+
+	friend std::ostream& operator<<(std::ostream& stream, const Matrix &m)  {
+		for (unsigned int i = 0; i < m.rows; i++){
+			stream << endl;
+			for (unsigned int j = 0; j < m.columns; j++){
+				stream << " | " << m.cells[i*m.columns + j] << " | ";
+			}
+		}
+
+
+		return stream;
+	}
+
+	Matrix &Matrix::operator=(const Matrix &other) {
+		if (this->columns != other.columns || this->rows != other.rows){
+			return *this;
+		}
+		else{
+			for (unsigned int i = 0; i < this->rows; i++){
+				for (unsigned int j = 0; j < this->columns; j++){
+					this->cells[i*this->columns + j] = other.cells[i*this->columns + j];
+				}
+			}
+			return *this;
+		}
+	}
+
+	bool Matrix::operator==(const Matrix &other) {
+		if (this->columns != other.columns || this->rows != other.rows){
+			return false;
+		}
+		for (unsigned int i = 0; i < this->rows; i++){
+			for (unsigned int j = 0; j < this->columns; j++){
+				if (this->cells[i*this->columns + j] == other.cells[i*this->columns + j])
+				{
+					//fine
+				}
+				else { return false; }
+			}
+		}
+		return true;
+	}
+
+	long* Matrix::operator[](int j){
+		long* result;
+		result = &this->cells[j*this->columns];
+		return result;
+	}
+
 
 
 
 private:
-	int* cells;
+	long* cells;
 	int rows;
 	int columns;
 };
 
-int input_int() {
-	std::string line;
-	int val;
-	while (std::getline(std::cin, line)) {
-		std::stringstream ss(line);
-		if (ss >> val) {
-			if (ss.eof()) {
-				break; // Success
-			}
-		}
+void getInputIntegral(long &number){
+	number = 0;
+	while (!(cin >> number)){
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Invalid input.  Try again: ";
 	}
-	return val;
 }
+
 
 int main(int argc, char **argv)
 {
-	int a = input_int();
-	int b = input_int();
+	long a = 0;
+	long b = 0;
+	getInputIntegral(a);
+	getInputIntegral(b);
 
 	Matrix m = Matrix(a, b);
+
+	m[0][0] = 37;
+	m[0][1] = 42;
+	m[3][3] = 32;
 	cout << m << endl;
 
 	return 0;
