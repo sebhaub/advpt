be4f3225f06e2f818887fe9f4d969e1b2de545f8
Sebastian Haubner <sebastian.haubner@fau.de>
2014-11-11 11:30:24 +0100

    extended bla

--- advpt-uebung1/cpp-matproduct/matproduct.cpp
+++ advpt-uebung1/cpp-matproduct/matproduct.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
-#include <assert>
+#include <cassert>
+#include <stdio.h>
+
+using namespace std;
 
 class Matrix{
 	public:
+		Matrix(int rows, int cols){
+			assert(rows > 0 && cols > 0);
+			this->rows = rows;
+			this->columns = cols;
+		}
 	
 	protected:
 	
 	private:
-		int* _data;
+		int* cells;
 		int rows;
-		int columns
-}
+		int columns;
+};
 
 
 
 int main(int argc, char **argv)
 {
+	cout << "hello world" << endl;
+	
+	int a, b;
+	
+	cin >> a;
+	cin >> b;
+	
+	Matrix m = Matrix(a, b);
 	
 	return 0;
 }
