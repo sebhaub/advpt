d9d6f4dfab18ca46f807943ca583e08ed617fabd
Jonas Gröger <jonas.groeger@gmail.com>
2014-11-11 11:57:13 +0100

    Nur format scheisse.

--- advpt-uebung1/cpp-matproduct/matproduct.cpp
+++ advpt-uebung1/cpp-matproduct/matproduct.cpp
@@ -7,38 +7,38 @@
 using namespace std;
 
 class Matrix{
-	public:
-		Matrix(int rows, int cols){
-			assert(rows > 0 && cols > 0);
-			this->rows = rows;
-			this->columns = cols;
-			this->cells = new int[rows*cols];
-			for(int i = 0; i < rows*cols; i++){
-				cells[i] = 2;
-			}
+public:
+	Matrix(int rows, int cols){
+		assert(rows > 0 && cols > 0);
+		this->rows = rows;
+		this->columns = cols;
+		this->cells = new int[rows*cols];
+		for (int i = 0; i < rows*cols; i++){
+			cells[i] = 2;
 		}
-		
-		~Matrix(){
-			delete[] cells;
-		}
-		
-		friend std::ostream& operator<<(std::ostream& os, const Matrix& obj)
-		{
-			for(int i = 0; i < obj.rows; i++){
-				for(int j = 0; j < obj.columns; j++){
-					os << obj.cells[i * obj.columns + j] + " a ";
-				}
-				os <<  "\n";
-			}
-			return os;
-		}
-		
+	}
 
-			
-	private:
-		int* cells;
-		int rows;
-		int columns;
+	~Matrix(){
+		delete[] cells;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Matrix& obj)
+	{
+		for (int i = 0; i < obj.rows; i++){
+			for (int j = 0; j < obj.columns; j++){
+				os << obj.cells[i * obj.columns + j] + " a ";
+			}
+			os << "\n";
+		}
+		return os;
+	}
+
+
+
+private:
+	int* cells;
+	int rows;
+	int columns;
 };
 
 int input_int() {
@@ -55,16 +55,14 @@ int input_int() {
 	return val;
 }
 
-
-
 int main(int argc, char **argv)
 {
 	int a = input_int();
 	int b = input_int();
-	
+
 	Matrix m = Matrix(a, b);
 	cout << m << endl;
-			
+
 	return 0;
 }
 
