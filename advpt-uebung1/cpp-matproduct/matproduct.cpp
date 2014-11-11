9ef7ae453d63f2b1d25fe07730eb146eaff98bbd
Sebastian Haubner <sebastian.haubner@fau.de>
2014-11-11 11:19:47 +0100

    Add matproduct.cpp

---
+++ advpt-uebung1/cpp-matproduct/matproduct.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <assert>
+
+class Matrix{
+	public:
+	
+	protected:
+	
+	private:
+		int* _data;
+		int rows;
+		int columns
+}
+
+
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
